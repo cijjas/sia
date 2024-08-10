a4d173eef71b920a675dd64a5edab1552251a729
Francisco <fferrutti@itba.edu.ar>
2024-08-10 15:06:48 -0300

    fix: ej2a. Missing error bars for 2b

--- tp0/ej2a.py
+++ tp0/ej2a.py
@@ -4,44 +4,107 @@ import csv
 import os
 import matplotlib.pyplot as plt
 import pandas as pd
-import seaborn as sns
+import numpy as np
 from src.catching import attempt_catch
 from src.pokemon import PokemonFactory, StatusEffect
+from scipy.interpolate import make_interp_spline
 
-def analyze_2a():
+LEVEL_MAX = 100
+HEALTH_MAX = 1
+SAVE_PATH = 'output/analyze/'
+EJ1A = '1a'
+EJ1B = '1b'
+EJ2A = '2a'
+EJ2B = '2b'
+EJ2C = '2c'
+
+def load_2a_health(pokemonName, pokeballs, noise=0):
+    with open('output/2a/health/2a.csv', mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['Pokeball','Pokemon', 'Probability', 'Captured', 'Current HP'])
+        divisions = 20
+        tries = 1000
+        for pokeball in pokeballs:
+            for i in range(divisions, 0, -1):
+                for j in range(tries):
+                    pokemon = factory.create( pokemonName, LEVEL_MAX, StatusEffect.NONE, i/divisions)
+                    writer.writerow([pokeball, pokemon.name,  attempt_catch(pokemon, pokeball, noise)[1], attempt_catch(pokemon, pokeball, noise)[0], i/divisions])
+
+def load_2a_status(pokemonName, pokeballs, noise=0):
+    with open(f"output/2a/status/{pokemonName}.csv", mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['Pokeball','Pokemon', 'Probability', 'Captured', 'Status'])
+        tries = 1000
+        for pokeball in pokeballs:
+            for status in StatusEffect.__members__.values():
+                for i in range(tries):
+                    pokemon = factory.create( pokemonName, LEVEL_MAX, status, HEALTH_MAX)
+                    writer.writerow([pokeball, pokemon.name,  attempt_catch(pokemon, pokeball, noise)[1], attempt_catch(pokemon, pokeball, noise)[0], status._name_])
+
+def analyze_2a_status():
+        data = pd.read_csv(f"output/2a/status/{os.listdir('output/2a/status').pop()}")
+        pokemon = data['Pokemon'].unique()
+        #title = '2a. Efecto de la salud sobre la efectividad de captura ' + ', '.join(pokemon)
+        data['Probability'] = pd.to_numeric(data['Probability'], errors='coerce')
+        
+        capture_mean = data.groupby(['Status', 'Pokeball'])['Probability'].mean().unstack()
+
+        plt.figure(figsize=(12, 8))
+        markers = ['o', '^', 's', 'p'] 
+        line_styles = ['-', '--', '-.', ':']
+        for idx, pokeball in enumerate(capture_mean.columns):
+            plt.plot(capture_mean.index, capture_mean[pokeball], 
+                    marker=markers[idx % len(markers)], 
+                    linestyle=line_styles[idx % len(line_styles)], 
+                    label=str(pokeball))
+        #plt.title(title)
+        plt.xlabel('Salud', fontsize=16)
+        plt.ylabel('Probabilidad de captura', fontsize=16)
+        plt.legend(title='Pokeball', loc='upper center', fontsize=14, title_fontsize=16)  
+        plt.grid(True)
+        plt.xticks(fontsize=16)
+        plt.yticks(fontsize=16)
+        plt.tight_layout()
+        plt.savefig(SAVE_PATH + EJ2A + '/2a_status.png')
+        plt.close()
+
+def analyze_2a_health():
     # Load data from CSV
-    data = pd.read_csv('output/2a.csv')
+    data = pd.read_csv(f"output/2a/health/2a.csv")
     pokemon = data['Pokemon'].unique()
-    title = 'Efecto de la vida (HP) en la probabilidad de captura para ' + ', '.join(pokemon)
-    data['Probability'] = pd.to_numeric(data['Probability'], errors='coerce')
+    #title = '2a. Efecto de la salud sobre la efectividad de captura ' + ', '.join(pokemon)
     
-    data.set_index(['Current HP', 'Pokeball'], inplace=True)
-    hp_effect = data['Probability'].unstack()
+    capture_mean = data.groupby(['Current HP', 'Pokeball'])['Captured'].mean().unstack()
 
-    # Plotting
     plt.figure(figsize=(12, 8))
     markers = ['o', '^', 's', 'p'] 
     line_styles = ['-', '--', '-.', ':']
-    for idx, pokeball in enumerate(hp_effect.columns):
-        plt.plot(hp_effect.index, hp_effect[pokeball], 
-                 marker=markers[idx % len(markers)], 
+    for idx, pokeball in enumerate(capture_mean.columns):
+        x = capture_mean.index
+        y = capture_mean[pokeball]
+        
+        # Interpolation for smooth lines
+        x_smooth = np.linspace(x.min(), x.max(), 300)
+        spl = make_interp_spline(x, y, k=3)  # k=3 for cubic spline
+        y_smooth = spl(x_smooth)
+        
+        plt.plot(x_smooth, y_smooth, 
                  linestyle=line_styles[idx % len(line_styles)], 
                  label=str(pokeball))
-    plt.title(title )
-    plt.xlabel('HP del pokemon')
-    plt.ylabel('Probabilidad de captura')
-    plt.legend(title='Pokeball', loc='upper right')  
-    plt.grid(True)
-    plt.show()
+        plt.scatter(x, y, 
+                    marker=markers[idx % len(markers)], 
+                    label=str(pokeball))
 
-def load_2a(pokemonName, pokeballs, noise=0):
-    with open('output/2a.csv', mode='w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(['Pokeball','Pokemon', 'Probability', 'Current HP'])
-        for pokeball in pokeballs:
-            for i in range(100, 0, -1):  
-                pokemon = factory.create( pokemonName, 100, StatusEffect.NONE, i/100)
-                writer.writerow([pokeball, pokemon.name,  attempt_catch(pokemon, pokeball, noise)[1], i/100])
+    #plt.title(title, fontsize=20)
+    plt.xlabel('HP', fontsize=16)
+    plt.ylabel('Probabilidad de captura', fontsize=16)
+    plt.legend(title='Pokeball', loc='upper right', fontsize=14, title_fontsize=16)  
+    plt.grid(True)
+    plt.xticks(fontsize=16)
+    plt.yticks(fontsize=16)
+    plt.tight_layout()
+    plt.savefig(SAVE_PATH + EJ2A + '/2a_health.png')
+    plt.close()
 
 
 if __name__ == "__main__":
@@ -51,7 +114,10 @@ if __name__ == "__main__":
     for pokemon_json in pokemon_configs:
         with open(f"{config_dir}/{pokemon_json}", "r") as f:
             config = json.load(f)
-            # 2.a Para un pokemon en especifico calcular la probabilidad de captura para cada pokeball en funcion de la vida del pokemon
-            load_2a(config["pokemon"], config["pokeballs"], 0.15)
-    analyze_2a()
-    
+            pokemon = config["pokemon"]
+            pokeballs = config["pokeballs"]
+            load_2a_health(pokemon, pokeballs)
+            load_2a_status(pokemon, pokeballs)
+
+    analyze_2a_health()
+    analyze_2a_status()
--- tp0/main.py
+++ tp0/main.py
@@ -8,6 +8,7 @@ import numpy as np
 import seaborn as sns
 from src.catching import attempt_catch
 from matplotlib.colors import LinearSegmentedColormap  # Add this import
+from scipy.interpolate import make_interp_spline
 
 from src.pokemon import PokemonFactory, StatusEffect
 
@@ -129,14 +130,17 @@ def analyze_1b():
     plt.close()
 
 
-def load_2a(pokemonName, pokeballs, noise=0):
-    with open('output/2a.csv', mode='w', newline='') as file:
+def load_2a_health(pokemonName, pokeballs, noise=0):
+    with open('output/2a/health/2a.csv', mode='w', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow(['Pokeball','Pokemon', 'Probability', 'Current HP'])
+        writer.writerow(['Pokeball','Pokemon', 'Probability', 'Captured', 'Current HP'])
+        divisions = 20
+        tries = 1000
         for pokeball in pokeballs:
-            for i in range(100, 0, -1):  
-                pokemon = factory.create( pokemonName, LEVEL_MAX, StatusEffect.NONE, i/100)
-                writer.writerow([pokeball, pokemon.name,  attempt_catch(pokemon, pokeball, noise)[1], i/100])
+            for i in range(divisions, 0, -1):
+                for j in range(tries):
+                    pokemon = factory.create( pokemonName, LEVEL_MAX, StatusEffect.NONE, i/divisions)
+                    writer.writerow([pokeball, pokemon.name,  attempt_catch(pokemon, pokeball, noise)[1], attempt_catch(pokemon, pokeball, noise)[0], i/divisions])
 
 def load_2b(pokemonName, pokeballs, noise=0):
     with open(f"output/2b/{pokemonName}.csv", mode='w', newline='') as file:
@@ -176,39 +180,81 @@ def analyze_2b():
     plt.close()
 
 
-def load_2a(pokemonName, pokeballs, noise=0):
-    with open(f"output/2a/{pokemonName}.csv", mode='w', newline='') as file:
+def load_2a_status(pokemonName, pokeballs, noise=0):
+    with open(f"output/2a/status/{pokemonName}.csv", mode='w', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow(['Pokeball','Pokemon', 'Probability', 'Status'])
+        writer.writerow(['Pokeball','Pokemon', 'Probability', 'Captured', 'Status'])
+        tries = 1000
         for pokeball in pokeballs:
-            for status in StatusEffect.__members__.values():  
-                pokemon = factory.create( pokemonName, LEVEL_MAX, status, HEALTH_MAX)
-                writer.writerow([pokeball, pokemon.name,  attempt_catch(pokemon, pokeball, noise)[1], status._name_])
+            for status in StatusEffect.__members__.values():
+                for i in range(tries):
+                    pokemon = factory.create( pokemonName, LEVEL_MAX, status, HEALTH_MAX)
+                    writer.writerow([pokeball, pokemon.name,  attempt_catch(pokemon, pokeball, noise)[1], attempt_catch(pokemon, pokeball, noise)[0], status._name_])
 
-def analyze_2a():
-        data = pd.read_csv(f"output/2a/{os.listdir('output/2a').pop()}")
+def analyze_2a_status():
+        data = pd.read_csv(f"output/2a/status/{os.listdir('output/2a/status').pop()}")
         pokemon = data['Pokemon'].unique()
-        title = '2a. Efecto de la salud sobre la efectividad de captura ' + ', '.join(pokemon)
+        #title = '2a. Efecto de la salud sobre la efectividad de captura ' + ', '.join(pokemon)
         data['Probability'] = pd.to_numeric(data['Probability'], errors='coerce')
-
-        data.set_index(['Status', 'Pokeball'], inplace=True) 
-        hp_effect = data['Probability'].unstack()
+        
+        capture_mean = data.groupby(['Status', 'Pokeball'])['Probability'].mean().unstack()
 
         plt.figure(figsize=(12, 8))
         markers = ['o', '^', 's', 'p'] 
         line_styles = ['-', '--', '-.', ':']
-        for idx, pokeball in enumerate(hp_effect.columns):
-            plt.plot(hp_effect.index, hp_effect[pokeball], 
+        for idx, pokeball in enumerate(capture_mean.columns):
+            plt.plot(capture_mean.index, capture_mean[pokeball], 
                     marker=markers[idx % len(markers)], 
                     linestyle=line_styles[idx % len(line_styles)], 
                     label=str(pokeball))
-        plt.title(title )
-        plt.xlabel('Estado de salud del Pokemon')
-        plt.ylabel('Probabilidad de captura')
-        plt.legend(title='Pokeball', loc='upper center')  
+        #plt.title(title)
+        plt.xlabel('Salud', fontsize=16)
+        plt.ylabel('Probabilidad de captura', fontsize=16)
+        plt.legend(title='Pokeball', loc='upper center', fontsize=14, title_fontsize=16)  
         plt.grid(True)
-        plt.savefig(SAVE_PATH + EJ2A+ '/2a.png')
+        plt.xticks(fontsize=16)
+        plt.yticks(fontsize=16)
+        plt.tight_layout()
+        plt.savefig(SAVE_PATH + EJ2A + '/2a_status.png')
         plt.close()
+
+def analyze_2a_health():
+    # Load data from CSV
+    data = pd.read_csv(f"output/2a/health/2a.csv")
+    pokemon = data['Pokemon'].unique()
+    #title = '2a. Efecto de la salud sobre la efectividad de captura ' + ', '.join(pokemon)
+    
+    capture_mean = data.groupby(['Current HP', 'Pokeball'])['Captured'].mean().unstack()
+
+    plt.figure(figsize=(12, 8))
+    markers = ['o', '^', 's', 'p'] 
+    line_styles = ['-', '--', '-.', ':']
+    for idx, pokeball in enumerate(capture_mean.columns):
+        x = capture_mean.index
+        y = capture_mean[pokeball]
+        
+        # Interpolation for smooth lines
+        x_smooth = np.linspace(x.min(), x.max(), 300)
+        spl = make_interp_spline(x, y, k=3)  # k=3 for cubic spline
+        y_smooth = spl(x_smooth)
+        
+        plt.plot(x_smooth, y_smooth, 
+                 linestyle=line_styles[idx % len(line_styles)], 
+                 label=str(pokeball))
+        plt.scatter(x, y, 
+                    marker=markers[idx % len(markers)], 
+                    label=str(pokeball))
+
+    #plt.title(title, fontsize=20)
+    plt.xlabel('HP', fontsize=16)
+    plt.ylabel('Probabilidad de captura', fontsize=16)
+    plt.legend(title='Pokeball', loc='upper right', fontsize=14, title_fontsize=16)  
+    plt.grid(True)
+    plt.xticks(fontsize=16)
+    plt.yticks(fontsize=16)
+    plt.tight_layout()
+    plt.savefig(SAVE_PATH + EJ2A + '/2a_health.png')
+    plt.close()
 
 def analyze_2c():
     all_data = pd.DataFrame()
@@ -368,14 +414,16 @@ if __name__ == "__main__":
             pokeballs = config["pokeballs"]
             load_1(pokemon, pokeballs, directory='output/1a')
             load_1(pokemon, pokeballs, directory='output/1b', reps=100000)
-            load_2a(pokemon, pokeballs)
+            load_2a_health(pokemon, pokeballs)
+            load_2a_status(pokemon, pokeballs)
             load_2b(pokemon, pokeballs)
-            # load_2c(pokemon, pokeballs)
+            load_2c(pokemon, pokeballs)
             load_2c_bis(pokemon, pokeballs)
     
     analyze_1a()
     analyze_1b()
-    analyze_2a()
+    analyze_2a_health()
+    analyze_2a_status()
     analyze_2b()
     analyze_2c_bis()
-    # analyze_2c()
+    analyze_2c()
