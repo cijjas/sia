32327de0bcb4d8d81f79ecfe815369c58c0ac324
cijjas <cijjas@itba.edu.ar>
2024-08-16 23:06:08 -0300

    chore: clean up

--- tp1/core/functions.py
+++
@@ -1,5 +0,0 @@
-def manhattan(point1, point2):
-    return abs(point1[0] - point2[0]) + abs(point1[1] - point2[1])
-
-def euclidean(point1, point2):
-    return ((point1[0] - point2[0])**2 + (point1[1] - point2[1])**2)**0.5
