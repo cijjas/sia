d3fd186398cace35b3f2ce168cdf43abc1c7ea2b
Francisco <fferrutti@itba.edu.ar>
2024-08-17 00:38:19 -0300

    Feat: first approach search_engine

---
+++ tp1/core/search_engine.py
@@ -0,0 +1,37 @@
+from core.algorithms import Algorithm
+from core.state import State
+from typing import Optional
+from time import sleep
+
+def get_children(state: State) -> list[State]:
+    children = []
+    for action in state.get_actions():
+        new_state = state.move_player(*action)
+        if new_state is not None:
+            children.append(new_state)
+    return children
+
+def search(algorithm: Algorithm, state: State, draw_board: Optional[callable] = None) -> Optional[State]:
+    explored = set()
+    algorithm.put(state)
+
+    while not algorithm.is_empty():
+
+        current = algorithm.get()
+        
+        if current.is_goal():
+            return current
+        
+        if current not in explored:
+            explored.add(current)
+            
+            for child in get_children(current):
+                if child not in explored:
+                    algorithm.put(child)
+        
+        if draw_board is not None:
+            draw_board(current)
+        
+        sleep(0.02)
+
+    return None
