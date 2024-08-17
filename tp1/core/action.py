efd83cd9db5cf868d46bede6a74d456d51cf21dd
cijjas <cijjas@itba.edu.ar>
2024-08-16 23:06:08 -0300

    chore: clean up

--- tp1/core/action.py
+++
@@ -1,6 +0,0 @@
-class Action:
-    def __init__(self, name, target1, target2, player_position):
-        self.name = name
-        self.target1 = target1
-        self.target2 = target2
-        self.player_position = player_position
