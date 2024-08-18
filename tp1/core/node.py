19178bc0ad67d87f56c722573ccb4021b8248925
cijjas <cijjas@itba.edu.ar>
2024-08-18 10:04:33 -0300

    fix: solve bfs repeating nodes issue and make more abstractions

--- tp1/core/node.py
+++ tp1/core/node.py
@@ -1,3 +1,4 @@
+from core.state import State
 class Node:
     def __init__(self, state, parent=None, action=None, path_cost=0, heuristic=0):
         self.state = state
@@ -21,6 +22,19 @@ class Node:
 
     def __str__(self):
         return str(self.state)
+
+    def get_children(self):
+        children = []
+        actions, states = self.state.get_actions()
+        for action, state in zip(actions, states):
+            children.append(Node(state, self, action, self.path_cost + 1))
+        return children
+
+    def is_goal(self):
+        return self.state.is_goal()
+
+    def is_dead_end(self):
+        return self.state.is_deadlock()
 
     def __eq__(self, other):
         return self.state == other.state
--- tp1/core/state.py
+++ tp1/core/state.py
@@ -4,47 +4,35 @@ class State:
         self.goals = goals
         self.boxes = boxes
         self.player = player
+        self._hash_value = None
 
-    def retrieve(self, x, y) -> str:
-        if (x, y) in self.walls: # O(1)
-            return 'wall'
-        elif (x, y) in self.goals:
-            return 'goal'
-        elif (x, y) in self.boxes:
-            return 'box'
-        elif (x, y) == self.player:
-            return 'player'
-        else:
-            return None
 
-    def is_in_corner(self, x, y):
-        north = self.retrieve(x, y - 1)
-        south = self.retrieve(x, y + 1)
-        east = self.retrieve(x + 1, y)
-        west = self.retrieve(x - 1, y)
-
-        # we define a a circular list to check the corners
-        neighbors = [north, east, south, west]
-
-        # if there are two walls in a row, we have a corner
-        for i in range(4):
-            if neighbors[i%4] == 'wall' and neighbors[(i + 1)%4] == 'wall':
-                return True
 
     def __eq__(self, other):
         return self.boxes == other.boxes and self.player == other.player
-
+    
     def __hash__(self):
-        return hash((tuple(self.boxes), self.player))
+        if self._hash_value is None:
+            self._hash_value = hash((tuple(self.boxes), self.player))
+        return self._hash_value
 
-    def __str__(self):
-        objects = {
-            'walls': self.walls,
-            'goals': self.goals,
-            'boxes': self.boxes,
-            'player': self.player
-        }
-        return str(objects)
+    
+
+    def print_state(self):
+        for y in range(0, 6):
+            for x in range(0, 6):
+                if (x, y) in self.walls:
+                    print('#', end='')
+                elif (x, y) in self.goals:
+                    print('.', end='')
+                elif (x, y) in self.boxes:
+                    print('$', end='')
+                elif (x, y) == self.player:
+                    print('@', end='')
+                else:
+                    print(' ', end='')
+            print()
+
 
     def can_move(self, dx, dy):
         x, y = self.player
@@ -56,7 +44,7 @@ class State:
             if (new_box_x, new_box_y) in self.walls or (new_box_x, new_box_y) in self.boxes:
                 return False
         return True
-
+    
     def move_player(self, dx, dy):
         if not self.can_move(dx, dy):
             return None
@@ -67,16 +55,32 @@ class State:
             new_box = (new_player[0] + dx, new_player[1] + dy)
             new_boxes.add(new_box)
         return State(self.walls, self.goals, new_boxes, new_player)
-
+    
     def is_goal(self):
         return all(box in self.goals for box in self.boxes)
-
+    
     def get_actions(self):
         actions = []
+        states = []
         for dx, dy in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
             if self.can_move(dx, dy):
+                states.append(self.move_player(dx, dy))
                 actions.append((dx, dy))
-        return actions
+        return actions, states
 
     def __str__(self):
         return f'State (\'{self.boxes}\', {self.player})'
+    
+    def is_deadlock(self):
+        return self._corner_deadlock()
+
+    def _corner_deadlock(self):
+        
+        for box in self.boxes:
+            if box not in self.goals:
+                x, y = box
+                if ((x - 1, y) in self.walls or (x + 1, y) in self.walls) and \
+                   ((x, y - 1) in self.walls or (x, y + 1) in self.walls):
+                    return True
+        return False
+    
