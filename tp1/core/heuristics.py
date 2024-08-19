b6cc880e83bbed894fc26485a68cf69a30d59cfa
Francisco <fferrutti@itba.edu.ar>
2024-08-18 23:08:27 -0300

    Feat: new heuristics

--- tp1/core/heuristics.py
+++ tp1/core/heuristics.py
@@ -45,3 +45,30 @@ class DeadlockCorner(Heuristic):
         if state.is_deadlock():
             return float('inf')
         return MinManhattan()(state)        
+
+# We can say that given a wall with no goals or holes, it is a deadlock if a box is next to it
+class DeadlockWall(Heuristic):
+    
+    def __call__(self, state: State) -> float:
+        # if a box is next to a wall
+        if not state.any_box_next_to_wall():
+            return MinManhattan()(state)
+        
+        for box, wall in state.get_boxes_next_to_wall():
+            if state.no_salvation_from_wall(box, wall):
+                return float('inf')
+        return MinManhattan()(state)
+
+class MinManhattanBetweenPlayerAndBox(Heuristic):
+    """ Returns the sum of the manhattan distances between each player and its closest box """
+    def __call__(self, state: State) -> float:
+        total_distance = 0
+        for box in state.boxes:
+            min_distance = float('inf')
+            for player in state.player:
+                distance = manhattan(box, player)
+                if distance < min_distance:
+                    min_distance = distance
+            total_distance += min_distance
+        return total_distance
+    
