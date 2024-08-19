71b4a8cb73fcdb44794ef917d1f4826a72f952e4
Francisco <fferrutti@itba.edu.ar>
2024-08-18 23:08:27 -0300

    Feat: new heuristics

--- tp1/core/state.py
+++ tp1/core/state.py
@@ -1,3 +1,5 @@
+from typing import List, Tuple
+
 class State:
     def __init__(self, walls: set, goals: set, boxes: set, player):
         self.walls = walls
@@ -66,6 +68,83 @@ class State:
                 states.append(self.move_player(dx, dy))
                 actions.append((dx, dy))
         return actions, states
+
+    def any_box_next_to_wall(self) -> bool:
+        for box in self.boxes:
+            if self.box_next_to_wall(box) is not None:
+                return True
+        return False
+
+    def box_next_to_wall(self, box) -> tuple[int, int]:
+        """ Returns the wall location. None if there is no wall next to the box """
+        x, y = box
+        if (x - 1, y) in self.walls:
+            return (x - 1, y)
+        if (x + 1, y) in self.walls:
+            return (x + 1, y)
+        if (x, y - 1) in self.walls:
+            return (x, y - 1)
+        if (x, y + 1) in self.walls:
+            return (x, y + 1)
+        return None
+    
+    def get_boxes_next_to_wall(self) -> List[Tuple[Tuple[int, int], Tuple[int, int]]]:
+        """ Returns a tuple with the box and the wall next to it. None if there is no box next to a wall """
+        boxes = []
+        for box in self.boxes:
+            wall = self.box_next_to_wall(box)
+            if wall is not None:
+                boxes.append((box, wall))
+        return boxes
+
+
+    def check_goals_along_wall(self, x, y, dir_parallel_to_wall):
+        """Helper function to check for goals along the wall in a given direction."""
+        while self.can_move(x, y):
+            if (x, y) in self.goals:
+                return True
+            x += dir_parallel_to_wall[0]
+            y += dir_parallel_to_wall[1]
+        return False
+
+    def check_walls_along_wall(self, wall_x, wall_y, dir_parallel_to_wall, dir_perpendicular_to_wall):
+        """Helper function to check for walls along the wall in a given direction."""
+        while (wall_x, wall_y) in self.walls:
+            wall_x += dir_parallel_to_wall[0]
+            wall_y += dir_parallel_to_wall[1]
+        dir_x, dir_y = wall_x, wall_y
+        dir_x -= dir_perpendicular_to_wall[0]
+        dir_y -= dir_perpendicular_to_wall[1]
+        return (dir_x, dir_y) in self.walls
+
+    # Looks for situations like these:
+    #               ##
+    #   ########    #
+    #   #  $   #    #$
+    #               #
+    #               ##
+    # it considers goals and holes in the middle
+    def no_salvation_from_wall(self, box, wall) -> bool:
+        """Returns True if there is no route to escape the box from the wall"""
+        x, y = box
+        wall_x, wall_y = wall
+        
+        dir_perpendicular_to_wall = (wall_x - x, wall_y - y)
+        dir_parallel_to_wall = (dir_perpendicular_to_wall[1], dir_perpendicular_to_wall[0])
+        
+        # Check for goals along the wall in both directions
+        if self.check_goals_along_wall(x, y, dir_parallel_to_wall):
+            return False
+        if self.check_goals_along_wall(x, y, (-dir_parallel_to_wall[0], -dir_parallel_to_wall[1])):
+            return False
+        
+        # Check for walls along the wall in both directions
+        if self.check_walls_along_wall(wall_x, wall_y, dir_parallel_to_wall, dir_perpendicular_to_wall):
+            return True
+        if self.check_walls_along_wall(wall_x, wall_y, (-dir_parallel_to_wall[0], -dir_parallel_to_wall[1]), dir_perpendicular_to_wall):
+            return True
+        
+        return False
 
     def __str__(self):
         return f'State (\'{self.boxes}\', {self.player})'
