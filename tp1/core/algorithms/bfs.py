f94e8afd1dd5c497c3b66888527f95def410f33b
JoacoGirod <joaquingirod@gmail.com>
2024-08-22 12:38:26 -0300

    fix: optimize all algorithms!

--- tp1/core/algorithms/bfs.py
+++ tp1/core/algorithms/bfs.py
@@ -1,15 +1,14 @@
 from collections import deque
 from core.models.node import Node
 
-# expanded nodes could be after the for in children
-# cuando llega a goal no suma el nodo goal a los explorados, ni suma los expanded_nodes
-# agregar a la frontier los childrens (si la estructura ignora repetidos) deberia ser equivalente a la logica actual de loopear por los children
-
 # Preference up, right, down, left
 def bfs(start_node=Node):
-    expanded_nodes = 0
-    frontier = deque([start_node])
+    frontier = deque()
     explored = set()
+    expanded_nodes = 0
+
+    frontier.append(start_node)
+    explored.add(start_node)
 
     while frontier:
         current_node = frontier.popleft()
@@ -17,11 +16,10 @@ def bfs(start_node=Node):
         if current_node.is_goal():
             return current_node.get_path(), expanded_nodes, len(frontier)
 
-        explored.add(current_node)
-
         for child in current_node.get_children():
-            if child not in explored and child not in frontier:
+            if child not in explored:
                 frontier.append(child)
+                explored.add(child)
 
         expanded_nodes += 1
 
--- tp1/core/algorithms/greedy_global.py
+++ tp1/core/algorithms/greedy_global.py
@@ -1,26 +1,31 @@
 from core.models.node import Node
 from core.models.state import State
 from core.heuristics import Heuristic
+from core.heuristics import heuristic_combinator
+import heapq
 
+# Similar to Local Greedy but the order is kept amongst all frontier nodes
+# Also similar A* but path cost is not taken into account
 def greedy_global(start_node: Node, heuristics=None):
-    expanded_nodes = 0
-    frontier = [start_node]
+    frontier = []
     explored = set()
+    expanded_nodes = 0
+
+    heapq.heappush(frontier, (0, start_node))
+    explored.add(start_node)
 
     while frontier:
-        current_node = min(frontier, key=lambda node: max(heuristic(node.state) for heuristic in heuristics))
-        frontier.remove(current_node)
+        _, current_node = heapq.heappop(frontier)
 
         if current_node.is_goal():
             return current_node.get_path(), expanded_nodes, len(frontier)
 
-        if current_node not in explored:
-            explored.add(current_node)
-
-            children = current_node.get_children()
-            for child in children:
-                if child not in explored and child not in frontier:
-                    frontier.append(child)
+        children = current_node.get_children()
+        for child in children:
+            if child not in explored:
+                current_h_score = heuristic_combinator(child, heuristics)
+                heapq.heappush(frontier, (current_h_score, child))
+                explored.add(child)
 
         expanded_nodes += 1
 
