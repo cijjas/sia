428a7f9e82222ac022eaa3bf708a46d664ef7d11
Francisco <fferrutti@itba.edu.ar>
2024-09-11 10:53:09 -0300

    Bug: normalizer divides by cero

--- tp2/src/graphic_generation.py
+++ tp2/src/graphic_generation.py
@@ -12,6 +12,7 @@ SELECTION_METHOD_COMPARISON_AVG_FILE_NAME = "selection_method_comparison.csv"
 SELECTION_METHOD_COMPARISON_BEST_FILE_NAME = "selection_method_comparison.csv"
 SELECTION_METHOD_COMPARISON_AVG_FILE_NAME_2 = "selection_method_comparison_2.csv"
 SELECTION_METHOD_COMPARISON_BEST_FILE_NAME_2 = "selection_method_comparison_2.csv"
+SELECTION_RATE_FILE_NAME = "selection_rate_comparison.csv"
 DATA_DIR = "../output/data"
 
 def selection_method_comparison(input_dir, is_avg=True):
@@ -104,11 +105,53 @@ def selection_method_comparison_2(input_dir, is_avg=True):
     wb.save(excel_filename)
     print(f"El heatmap se guardó en {excel_filename}")
 
+def selection_rate_comparison(input_dir, is_avg=True):
+    print("Generando heatmap para comparación de tasas de selección en base al fitness promedio de la generación 100")
+    
+    # Cargar el CSV
+    df = pd.read_csv(os.path.join(input_dir, SELECTION_RATE_FILE_NAME))
+
+    # Crear un pivot table
+    pivot_table = df.pivot_table(index='selection_rate', columns='selection_method', values='fitness', aggfunc='mean')
+
+    # Exportar a Excel
+    excel_filename = os.path.join(DATA_DIR, 'heatmap_selection_rate_avg.xlsx')
+
+    # Crear directorio si no existe
+    os.makedirs(DATA_DIR, exist_ok=True)
+    pivot_table.to_excel(excel_filename)
+
+    # Cargar el archivo de Excel para formatear el heatmap
+    wb = openpyxl.load_workbook(excel_filename)
+    ws = wb.active
+
+    # Crear el heatmap con formato condicional
+    # Obtener los rangos de los valores de fitness
+    min_val = pivot_table.min().min()
+    max_val = pivot_table.max().max()
+
+    # Aplicar colores en función del valor de fitness
+    for row in ws.iter_rows(min_row=2, min_col=2, max_row=ws.max_row, max_col=ws.max_column):
+        for cell in row:
+            if isinstance(cell.value, (float, int)):  # Solo aplicar formato a celdas con números
+                # Normalizar el valor
+                normalized_value = (cell.value - min_val) / (max_val - min_val)
+                # Generar el color (del rojo al verde)
+                red = int(255 * (1 - normalized_value))
+                green = int(255 * normalized_value)
+                fill_color = PatternFill(start_color=f'{red:02X}{green:02X}00', end_color=f'{red:02X}{green:02X}00', fill_type='solid')
+                cell.fill = fill_color
+
+    # Guardar el archivo de Excel con el heatmap
+    wb.save(excel_filename)
+    print(f"El heatmap se guardó en {excel_filename}")
+
 def main():
     selection_method_comparison(OUTPUT_DIR, is_avg=True)
     selection_method_comparison(OUTPUT_DIR, is_avg=False)
     selection_method_comparison_2(OUTPUT_DIR, is_avg=True)
     selection_method_comparison_2(OUTPUT_DIR, is_avg=False)
+
 
 if __name__ == "__main__":
     main()
--- tp2/src/selection_analysis.py
+++ tp2/src/selection_analysis.py
@@ -1,3 +1,4 @@
+import sys
 import csv
 import os
 from game.game import start_game
@@ -8,6 +9,7 @@ from genetic_algorithm.selection import get_selection_methods
 from genetic_algorithm.classes.hyperparameters import Hyperparameters, Selector
 
 OUTPUT_DIR = "../output"
+SELECTION_RATE_FILE_CONF = "../config/algorithm_config.json"
 
 def selection_method_comparison():
     """ Compare the selection methods using all available methods. Returns a csv with the best fitness, the average fitness and the execution time for each method."""
@@ -103,8 +105,52 @@ def selection_method_comparison_2():
 
     return
 
-if __name__ == "__main__":
-    selection_method_comparison()
-    selection_method_comparison_2()
+def selection_rate_analysis(config_file):
+    """ Compare the selection rate given a configuration. Returns a csv with the best fitness and the average fitness for each selection rate. """
+    game_config_file = "../config/game_config.json"
+
+    config_loader = ConfigLoader(config_file, game_config_file)
+    game_config = config_loader.load_game_config()
+    timer, points, character = start_game(game_config)
+
+    config: Hyperparameters = config_loader.load()
+
+    # create the output directory if it does not exist
+    if not os.path.exists(OUTPUT_DIR):
+        os.makedirs(OUTPUT_DIR)
+
+    output_file = f"{OUTPUT_DIR}/selection_rate_comparison.csv"
+
+    # Open the file once and write the header
+    with open(output_file, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["strength", "dexterity", "intelligence", "vigor", "constitution", "height", "fitness", "generation", "selection_rate"])
+
+    for selection_rate in range(1, 101):
+        config.selection_rate = selection_rate / 100
+
+        population = algorithm.run_genetic_algorithm(config, eve, timer, points, character)
+        
+        with open(output_file, "a", newline="") as f:  # Open the file in append mode
+            writer = csv.writer(f)
+            for individual in population.individuals:
+                writer.writerow([individual.genes.strength, individual.genes.dexterity, individual.genes.intelligence,
+                                individual.genes.vigor, individual.genes.constitution, individual.genes.height,
+                                individual.fitness, population.generation, selection_rate])
+
+    return
+
+def main():
+    if len(sys.argv) != 2:
+        print("Using default files")
+        args = ["", SELECTION_RATE_FILE_CONF]
+    else:
+        args = sys.argv
+    selection_rate_analysis(args[1])
+    #selection_method_comparison()
+    #selection_method_comparison_2()
     print("Done")
+
+if __name__ == "__main__":
+    main()
 
