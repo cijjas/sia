03b0c25ac9d28f8218b081dad1b6cb59841635d3
JoacoGirod <joaquingirod@gmail.com>
2024-09-17 11:06:00 -0300

    Fix : fix eve bug and redo outputs

--- tp2/src/brute_force/breaker.c
+++ tp2/src/brute_force/breaker.c
@@ -10,33 +10,36 @@
 #define MIN_POINTS 100
 #define MAX_POINTS 200
 
-// gcc - O3 - o breaker.out breaker.c - lm
+// gcc -O3 -o breaker.out breaker.c -lm
 
-// Precompute tanh lookuprtable for values from 0 to 200
+// Precompute tanh lookup table for values from 0 to 200
 double tanh_lookup[201];
 
 // Function prototypes
 void precompute_tanh_lookup();
 double eve(const char *character, int strength, int dexterity, int intelligence, int vigor, int constitution, double height);
-void write_best_distribution_to_csv(const char *filename, const char *character, double best_value, int *best_distribution, double height, int total_points);
+void write_best_distribution_to_csv(const char *filename, double best_value, int *best_distribution, double height, int total_points);
 void write_csv_header(const char *filename);
 
 int main()
 {
-
     precompute_tanh_lookup();
-    // printf("warrior : <%f>\n", eve("warrior", 50, 50, 0, 0, 0, 1.92));
-    // printf("archer : <%f>\n", eve("archer", 50, 50, 0, 0, 0, 1.92));
     const char *classes[] = {"warrior", "archer", "guardian", "mage"};
+    const char *filenames[] = {"warrior_best_values.csv", "archer_best_values.csv", "guardian_best_values.csv", "mage_best_values.csv"};
     int num_classes = sizeof(classes) / sizeof(classes[0]);
-    const char *filename = "best_values.csv";
-    write_csv_header(filename);
+
+    // Write headers for each class file
+    for (int c = 0; c < num_classes; ++c)
+    {
+        write_csv_header(filenames[c]);
+    }
 
     for (int total_points = MIN_POINTS; total_points <= MAX_POINTS; ++total_points)
     {
         for (int c = 0; c < num_classes; ++c)
         {
             const char *character = classes[c];
+            const char *filename = filenames[c];
             double best_value = -INFINITY;
             int best_distribution[NUM_FIELDS] = {0};
             double best_height = HEIGHT_START;
@@ -57,7 +60,6 @@ int main()
                                 double result = eve(character, distribution[0], distribution[1], distribution[2], distribution[3], distribution[4], height);
                                 if (result > best_value)
                                 {
-                                    // printf("%s,%d,%d,%d,%d,%d,%d,%f,%f\n", character, total_points, distribution[0], distribution[1], distribution[2], distribution[3], distribution[4], height, result);
                                     best_value = result;
                                     best_height = height;
                                     memcpy(best_distribution, distribution, sizeof(int) * NUM_FIELDS);
@@ -67,7 +69,7 @@ int main()
                     }
                 }
             }
-            write_best_distribution_to_csv(filename, character, best_value, best_distribution, best_height, total_points);
+            write_best_distribution_to_csv(filename, best_value, best_distribution, best_height, total_points);
         }
     }
     return 0;
@@ -83,7 +85,6 @@ void precompute_tanh_lookup()
 
 double eve(const char *character, int strength, int dexterity, int intelligence, int vigor, int constitution, double height)
 {
-    // Ensure that indices are within bounds
     if (strength < 0 || strength > 200 || dexterity < 0 || dexterity > 200 || intelligence < 0 || intelligence > 200 || vigor < 0 || vigor > 200 || constitution < 0 || constitution > 200)
     {
         return -INFINITY;
@@ -115,18 +116,18 @@ double eve(const char *character, int strength, int dexterity, int intelligence,
     }
     else if (strcmp(character, "mage") == 0)
     {
-        return 0.8 * attack + 0.2 * defense;
+        return 0.8 * attack + 0.3 * defense; // weird que este tirando los mismos numeros verificar contra el eve de python de ultima
     }
 
     return -INFINITY;
 }
 
-void write_best_distribution_to_csv(const char *filename, const char *character, double best_value, int *best_distribution, double height, int total_points)
+void write_best_distribution_to_csv(const char *filename, double best_value, int *best_distribution, double height, int total_points)
 {
     FILE *file = fopen(filename, "a");
     if (file != NULL)
     {
-        fprintf(file, "%s,%d,%d,%d,%d,%d,%d,%f,%f\n", character, total_points, best_distribution[0], best_distribution[1], best_distribution[2], best_distribution[3], best_distribution[4], height, best_value);
+        fprintf(file, "%d,%d,%d,%d,%d,%d,%f,%f\n", total_points, best_distribution[0], best_distribution[1], best_distribution[2], best_distribution[3], best_distribution[4], height, best_value);
         fclose(file);
     }
 }
@@ -136,7 +137,7 @@ void write_csv_header(const char *filename)
     FILE *file = fopen(filename, "w");
     if (file != NULL)
     {
-        fprintf(file, "Class,Total Points,Strength,Dexterity,Intelligence,Vigor,Constitution,Height,Value\n");
+        fprintf(file, "Total Points,Strength,Dexterity,Intelligence,Vigor,Constitution,Height,Value\n");
         fclose(file);
     }
 }
