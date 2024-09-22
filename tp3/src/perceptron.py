46b9d9eb540bd3a65b14567f3da2d03be627f687
Gianfranco Magliotti <gmagliotti@itba.edu.ar>
2024-09-22 18:30:51 -0300

    feat: initial SL perceptron implementation

---
+++ tp3/src/perceptron.py
@@ -0,0 +1,63 @@
+import numpy as np
+
+class Perceptron:
+    def __init__(self, num_inputs, learning_rate = 0.01, debug = False) -> None:
+        self.weights = np.random.rand(num_inputs + 1)
+        self.learning_rate = learning_rate
+        self.debug = debug
+        self.__THRESHOLD = 0.0
+        self.__EPSILON = 1e-5
+
+    def linear(self, inputs):
+        # Add a bias constant term to the inputs
+        inputs_with_bias = self.add_bias(inputs)
+        # @ is the matrix multiplication operator in Python
+        # equivalent to array.dot(other_array)
+        Z = inputs_with_bias @ self.weights
+        return Z
+    
+    def add_bias(self, inputs):
+        if inputs.ndim == 1:
+            return np.hstack(([1], inputs))
+        else:
+            return np.hstack((np.ones((inputs.shape[0], 1)), inputs))
+
+    # Define the Heaviside Step function.
+    def heaviside_step_fn(self, z):
+        return np.where(z >= self.__THRESHOLD, 1, -1)
+         
+    # Define the Prediction
+    def predict(self, inputs):
+        Z = self.linear(inputs)
+        return self.heaviside_step_fn(Z)
+     
+    # Define the Loss function
+    def loss(self, prediction, target):
+        loss = (target-prediction)
+        return loss
+     
+    #Define training
+    def train(self, inputs, target) -> float:
+        prediction = self.predict(inputs)
+        error = self.loss(prediction, target)
+        inputs_with_bias = self.add_bias(inputs)
+        self.weights += self.learning_rate * error * inputs_with_bias
+        return error
+         
+    # Fit the model
+    def fit(self, X, y, num_epochs):
+        total_error = 0
+        if self.debug:
+            print(f'Initial Weights: {self.weights}')
+        for epoch in range(num_epochs):
+            total_error = 0
+            for inputs, target in zip(X, y):
+                error = self.train(inputs, target)
+                total_error += np.abs(error)
+            if self.debug:
+                print(f'Epoch {epoch+1}/{num_epochs}, Total Error: {total_error}, Weights: {self.weights}')
+            if total_error < self.__EPSILON:
+                print(f'Converged after {epoch+1} epochs.')
+                break
+        else:
+            print(f'Did not converge after {num_epochs} epochs.')
