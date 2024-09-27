5026486e8a6a16d04968aa7b53f834ceddf11b32
cijjas <cijjas@itba.edu.ar>
2024-09-27 11:32:56 -0300

    fix: add res and fix .py exten

--- tp3/src/perceptron/perceptron_simple.py
+++ tp3/src/perceptron/perceptron_simple.py
@@ -1,14 +1,14 @@
 import numpy as np
 from typing import Callable
-from perceptron_base import PerceptronBase
+from perceptron.perceptron_base import PerceptronBase
 
-class PerceptronLinear(PerceptronBase):
+class PerceptronSimple(PerceptronBase):
     
     def __init__(self, num_inputs,  weights=None, learning_rate = 0.01, threshold: float = 0.0, epsilon: float = 1e-5) -> None:
         super().__init__(num_inputs, weights, learning_rate, threshold, epsilon)
         
     def compute_activation(self, h_mu):
-        return h_mu
+        return 1 if h_mu > 0 else -1
     
     def gradient(self, h_mu):
-        return 1.0
+        return 1
