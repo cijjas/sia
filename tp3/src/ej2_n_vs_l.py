236f42e087ea21446dbe2cff9636863e8a6e6d18
Francisco <fferrutti@itba.edu.ar>
2024-10-02 14:45:38 -0300

    Revert "Fix: ej2_n_vs_l.py"
    This reverts commit f89efa72694533bdeb7679b203f12188a2928c12.

--- tp3/src/ej2_n_vs_l.py
+++ tp3/src/ej2_n_vs_l.py
@@ -33,10 +33,7 @@ def main():
     beta = 2.0
     num_epochs = 1000
 
-    for i in range(n_splits):
-        test_index = folds[i]
-        train_index = np.concatenate([folds[j] for j in range(n_splits) if j != i])
-        
+    for train_index, test_index in folds.split(X_scaled):
         X_train, X_test = X_scaled[train_index], X_scaled[test_index]
         y_train, y_test = y_scaled[train_index], y_scaled[test_index]
         
