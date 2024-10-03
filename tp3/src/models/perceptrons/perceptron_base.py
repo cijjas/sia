c0d1a9cae2e25438e8795c63a5ccc2759b38b866
cijjas <cijjas@itba.edu.ar>
2024-10-03 12:12:32 -0300

    feat: add ej2 raphs

--- tp3/src/models/perceptrons/perceptron_base.py
+++ tp3/src/models/perceptrons/perceptron_base.py
@@ -1,14 +1,17 @@
 import numpy as np
-from typing import Callable
+from tqdm import tqdm
 
 class PerceptronBase:
-    def __init__(self,seed, num_features,  weights=None, learning_rate = 0.01, epsilon: float = 1e-5) -> None:
+    def __init__(self,seed, num_features,  weights=None, learning_rate = 0.01, epsilon: float = 1e-5, optimizer: bool= False) -> None:
         if seed is not None:
             np.random.seed(seed)
         self.weights = np.random.normal(0, 0.01, num_features +1) if weights is None else weights
         self.learning_rate = learning_rate
         self.weights_history = [self.weights.copy()]
         self.loss_history = []
+        self.beta_history = []
+        self.learning_rate_history = []
+        self.optimizer = optimizer
         self.epsilon = epsilon
 
 
@@ -31,7 +34,9 @@ class PerceptronBase:
 
         return O_h
     
-    
+    def compute_beta(self, y, o, h, ):
+        pass
+
     # exactamente copiado de lo que nos dio la catedra
     def train(self, X, y, num_epochs):
         converged = False
@@ -53,13 +58,18 @@ class PerceptronBase:
                 for i in range(len(self.weights)):
                     self.weights[i] += self.learning_rate * (y[mu] - O_h_mu) * X_mu[i] * self.compute_gradient(h_mu)
                 
+                # also optimize beta and eta
+                if self.optimizer:
+                    self.beta += self.beta_learning_rate * (y[mu] - O_h_mu) * h_mu * self.compute_gradient(h_mu)
+
+
                 expected.append(y[mu])
                 actual.append(O_h_mu)
 
             # 4. calculate the perceptron error
-            
             error = self.compute_error(expected, actual)
-            #print(f"------------------------------ Epoch {epoch+1}, Error: {error} ----------------------------------------")
+            if self.optimizer:
+                self.beta_history.append(self.beta)
             self.weights_history.append(self.weights.copy())
             self.loss_history.append(error)
             if error < self.epsilon:
@@ -67,9 +77,9 @@ class PerceptronBase:
                 break
 
         if not converged:
-            print(f"Perceptron did not converge in {num_epochs} epochs")
+            print(f"Perceptron did not converge in {num_epochs} epochs with error {error}")
         else:
-            print(f"Perceptron converged in {epoch+1} epochs")
+            print(f"Perceptron converged in {epoch+1} epochs with error {error}")
         
         return converged
 
