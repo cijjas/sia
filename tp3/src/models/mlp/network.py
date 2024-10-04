ecf0a2741cdde9759e0963302e9d181794b17366
JoacoGirod <joaquingirod@gmail.com>
2024-10-04 17:48:32 -0300

    chore : improve readability of network.py

--- tp3/src/models/mlp/network.py
+++ tp3/src/models/mlp/network.py
@@ -26,21 +26,23 @@ class MultilayerPerceptron(object):
 
         if seed is not None:
             np.random.seed(seed)
-        self.biases: list[np.ndarray] = [np.random.randn(y, 1) for y in topology[1:]] # crea vector de bias para cada capa
-        self.weights: list[np.ndarray] = [np.random.randn(y, x) for x, y in zip(topology[:-1], topology[1:])] # crea matriz de pesos para cada lazo
+
+        self.biases: list[np.ndarray] = [np.random.randn(y, 1) for y in topology[1:]] # Bias vector of matrixes ignores the input layer
+        self.weights: list[np.ndarray] = [np.random.randn(y, x) for x, y in zip(topology[:-1], topology[1:])] # Matches each layer with the next, ignoring the output layer
 
         self.num_layers: int = len(topology)
         self.topology: list[int] = topology
-
         self.activation_function = activation_function
         self.optimizer = optimizer
 
     def feedforward(self, a: np.ndarray) -> np.ndarray:
         """Return the output of the network if 'a' is the input."""
+
         for b, w in zip(self.biases, self.weights):
             a = self.activation_function.activation(np.dot(w, a) + b)
         return a
 
+    # TODO Remove test_data comment, parameter and logic, unnecesarry overhead
     def fit(self, training_data: list[tuple[np.ndarray, np.ndarray]], epochs: int, mini_batch_size: int, eta: float,
             epsilon: float, test_data: Optional[list[tuple[np.ndarray, np.ndarray]]] = None, test_results:list[tuple[int, int]] = None) -> None:
         """Train the neural network using mini-batch stochastic
@@ -63,7 +65,9 @@ class MultilayerPerceptron(object):
                 training_data[k:k+mini_batch_size]
                 for k in range(0, n, mini_batch_size)]
             for mini_batch in mini_batches:
-                self.update_mini_batch(mini_batch, eta)
+                self.update_weights_and_biases(mini_batch, eta)
+
+            # Logic for when test_data is provided
             if test_data is not None:
                 if test_results is not None:
                     test_result = []
@@ -76,17 +80,21 @@ class MultilayerPerceptron(object):
                 print("Epoch {0} complete".format(j))
 
 
-    def update_mini_batch(self, mini_batch: list[tuple[np.ndarray, np.ndarray]], eta: float) -> None:
+    def update_weights_and_biases(self, mini_batch: list[tuple[np.ndarray, np.ndarray]], eta: float) -> None:
         """Update the network's weights and biases by applying
         gradient descent using backpropagation to a single mini batch.
+
         The ``mini_batch`` is a list of tuples ``(x, y)``, and ``eta``
         is the learning rate."""
+
         nabla_b: list[np.ndarray] = [np.zeros(b.shape) for b in self.biases]
         nabla_w: list[np.ndarray] = [np.zeros(w.shape) for w in self.weights]
+
         for x, y in mini_batch:
             delta_nabla_b, delta_nabla_w = self.backprop(x, y)
             nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
             nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+
         self.weights, self.biases = self.optimizer.update(
             weights=self.weights,
             biases=self.biases,
@@ -95,38 +103,51 @@ class MultilayerPerceptron(object):
             mini_batch_size=len(mini_batch)
         )
 
-
-    def backprop(self, predicted:np.ndarray, expected:np.ndarray) -> tuple[list[np.ndarray], list[np.ndarray]]:
+    def backprop(self, input:np.ndarray, expected:np.ndarray) -> tuple[list[np.ndarray], list[np.ndarray]]:
         """Return a tuple ``(nabla_b, nabla_w)`` representing the
-        gradient for the cost function C_x.  ``nabla_b`` and
-        ``nabla_w`` are layer-by-layer lists of numpy arrays, similar
-        to ``self.biases`` and ``self.weights``."""
+        gradient for the cost function C_x.
+
+        ``nabla_b`` and ``nabla_w`` are layer-by-layer lists of numpy
+        arrays, similarto ``self.biases`` and ``self.weights``."""
+
         nabla_b: list[np.ndarray] = [np.zeros(b.shape) for b in self.biases]
         nabla_w: list[np.ndarray] = [np.zeros(w.shape) for w in self.weights]
-        # feedforward
-        activation: np.ndarray = predicted
-        # activations is a list of arrays, where each array is the activations of the neurons in that layer
-        activations: list[np.ndarray] = [predicted] # list to store all the activations, layer by layer
-        zs: list[np.ndarray] = [] # list to store all the z vectors, layer by layer
-        # Feed Foward that stores the values of the activations for later usage
+
+        activation: np.ndarray = input # Represents a^{(L-1)} when calculating a^{(L)}
+        activations: list[np.ndarray] = [input] # Stores all activations for later usage in Backpropagation
+        zs: list[np.ndarray] = [] # Stores all pre activations for later usage in Backpropagation
+
+        # Feed Foward with storage
         for b, w in zip(self.biases, self.weights):
+            # Preactivation calculation and storage for each layer
             z: np.ndarray = np.dot(w, activation) + b
             zs.append(z)
+            # Activation calculation and storage for each layer
             activation = self.activation_function.activation(z)
             activations.append(activation)
-        # Backpropagation
+
         # Last layer needs special treatment
+        # δ = ∂C/∂a^{(L)} ​⋅ ∂a^{(L)}/∂z^{(L)}
         delta: np.ndarray = self.cost_derivative(activations[-1], expected) * self.activation_function.activation_prime(zs[-1])
+        # ∇b = ∂C/∂b = δ because ∂z/∂b = 1
         nabla_b[-1] = delta
+        # ∇w = δ ⋅ ∂z/∂w = δ ⋅ activation^{(L-1)}.T
         nabla_w[-1] = np.dot(delta, activations[-2].T)
-        # Now we go from the second to last to the input layer
+
+        # Backpropagate the error
         for l in range(2, self.num_layers):
-            z: np.ndarray = zs[-l]
-            sp: np.ndarray = self.activation_function.activation_prime(z)
-            delta = np.dot(self.weights[-l+1].transpose(), delta) * sp
+            # δ = ∂C/∂z^{(l)} = ∂C/∂a^{(l)} ​⋅ ∂a^{(l)}/∂z^{(l)}
+            delta = np.dot(self.weights[-l+1].transpose(), delta) * self.activation_function.activation_prime(zs[-l])
+            # δ^{(l-1)} = ∂C/∂a^{(l-1)} = δ^{(l)} ⋅ W^{(l)}
             nabla_b[-l] = delta
+            # Gradient of the cost with respect to weights: ∂C/∂W^{(l)} = δ^{(l)} ⋅ a^{(l-1)}^T
             nabla_w[-l] = np.dot(delta, activations[-l-1].transpose())
         return (nabla_b, nabla_w)
+
+    def cost_derivative(self, output_activations: np.ndarray, y: np.ndarray) -> np.ndarray:
+        """Return the vector of partial derivatives partial C_x
+        partial a for the output activations."""
+        return (output_activations-y)
 
     def evaluate(self, test_data: list[tuple[np.ndarray, np.ndarray]], epsilon: float, test_results: list[tuple[np.ndarray, np.ndarray]] = None) -> float:
         if self.topology[-1] == 1:
@@ -134,16 +155,10 @@ class MultilayerPerceptron(object):
         else:
             return self.multi_output_evaluation(test_data, epsilon, test_results)
 
-    def cost_derivative(self, output_activations: np.ndarray, y: np.ndarray) -> np.ndarray:
-        """Return the vector of partial derivatives partial C_x
-        partial a for the output activations."""
-        return (output_activations-y)
 
     def single_output_evaluation(self, test_data: list[tuple[np.ndarray, np.ndarray]], epsilon: float, test_results: list[tuple[np.ndarray, np.ndarray]] = None) -> float:
         """Return the number of test inputs for which the neural
-        network outputs the correct result. Note that the neural
-        network's output is assumed to be the index of whichever
-        neuron in the final layer has the highest activation."""
+        network outputs the correct result."""
 
         # Initialize test_results if it's not provided
         if test_results is None:
@@ -166,9 +181,7 @@ class MultilayerPerceptron(object):
 
     def multi_output_evaluation(self, test_data: list[tuple[np.ndarray, np.ndarray]], epsilon: float, test_results: list[tuple[np.ndarray, np.ndarray]] = None) -> float:
         """Return the number of test inputs for which the neural
-        network outputs the correct result. Note that the neural
-        network's output is assumed to be the index of whichever
-        neuron in the final layer has the highest activation."""
+        network outputs the correct result."""
 
         # Initialize test_results if it's not provided
         if test_results is None:
