481f60eac8ac25e661d898e517ae9e6dc1c8e25a
cijjas <cijjas@itba.edu.ar>
2024-10-17 13:01:12 -0300

    fix: fix broken similarity functions, add epochs and comment

--- tp4/src/utils/radius_function.py
+++ tp4/src/utils/radius_function.py
@@ -1,14 +1,20 @@
 import numpy as np
 
 
-EXPONENTIAL_DECAY_RADIUS = 'exponential_decay'
-CONSTANT_RADIUS = 'constant'
+EXPONENTIAL_DECAY_RADIUS = "exponential_decay"
+CONSTANT_RADIUS = "constant"
 
-def exponential_decay_radius(radius, iteration, max_iterations) -> float:
-    return radius * np.exp(-iteration / max_iterations)
 
-def constant_radius(radius, iteration, max_iterations) -> float:
+# Constant radius
+def constant_radius(radius, iteration=None, max_iterations=None) -> float:
     return radius
+
+
+# Exponential Decay radius
+def exponential_decay_radius(radius, iteration, max_iterations) -> float:
+    return radius * np.exp(
+        -iteration / max_iterations
+    )  # R(t) = R(0) * exp(-t/T)  exponential decay of the radius
 
 
 def str_to_radius_function(radius_function) -> callable:
@@ -17,5 +23,4 @@ def str_to_radius_function(radius_function) -> callable:
     elif radius_function == CONSTANT_RADIUS:
         return constant_radius
     else:
-        raise ValueError(f'Invalid radius function: {radius_function}')
-
+        raise ValueError(f"Invalid radius function: {radius_function}")
