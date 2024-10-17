45654857e21afca9e15f9220f41a2cd2e77b288a
cijjas <cijjas@itba.edu.ar>
2024-10-17 16:59:44 -0300

    chore: implement PCA with covariance matrix

--- tp3/src/ej1.py
+++ tp3/src/ej1.py
@@ -5,7 +5,13 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import imageio.v2 as imageio
 from io import BytesIO
-from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, f1_score
+from sklearn.metrics import (
+    confusion_matrix,
+    accuracy_score,
+    precision_score,
+    recall_score,
+    f1_score,
+)
 from sklearn.model_selection import KFold
 from models.perceptrons.perceptron_simple import PerceptronSimple
 
@@ -19,13 +25,14 @@ def plot_2d_projected_decision_boundary(weights, X, y, epoch, config, grid_range
     # Scatter plot for the data points
     for class_value in np.unique(y):
         row_ix = np.where(y == class_value)
-        ax.scatter(X[row_ix, 0], X[row_ix, 1], label=f'Class {class_value}')
+        ax.scatter(X[row_ix, 0], X[row_ix, 1], label=f"Class {class_value}")
 
     # Create a mesh grid for plotting the decision boundary
     x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
     x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, 0.1),
-                           np.arange(x2_min, x2_max, 0.1))
+    xx1, xx2 = np.meshgrid(
+        np.arange(x1_min, x1_max, 0.1), np.arange(x2_min, x2_max, 0.1)
+    )
 
     # Calculate the decision boundary (Z values)
     grid = np.c_[xx1.ravel(), xx2.ravel()]
@@ -41,16 +48,16 @@ def plot_2d_projected_decision_boundary(weights, X, y, epoch, config, grid_range
         slope = -(weights[1] / weights[2])
         intercept = -(weights[0] / weights[2])
         x_values = np.linspace(x1_min, x1_max, 100)
-        ax.plot(x_values, slope * x_values + intercept, 'k--')
+        ax.plot(x_values, slope * x_values + intercept, "k--")
 
     # Include hyperparameter information in the plot
     hyperparams_info = f"$\\eta$: {config.get('learning_rate', 0.01)}, Epochs: {config.get('epochs', 1)}"
-    ax.set_title(f'Epoch: {epoch+1} - {hyperparams_info}')
+    ax.set_title(f"Epoch: {epoch+1} - {hyperparams_info}")
     ax.legend()
 
     # Save plot to a bytes buffer
     buf = BytesIO()
-    plt.savefig(buf, format='png')
+    plt.savefig(buf, format="png")
     buf.seek(0)
     plt.close(fig)
     return buf
@@ -58,42 +65,42 @@ def plot_2d_projected_decision_boundary(weights, X, y, epoch, config, grid_range
 
 # The rest of your code remains unchanged
 
+
 def main():
     config_file = {}
     if len(sys.argv) > 1:
         try:
-            with open(sys.argv[1], 'r') as file:
+            with open(sys.argv[1], "r") as file:
                 config_file = json.load(file)
         except Exception as e:
             print(f"Failed to read configuration file: {e}")
             sys.exit(1)
 
     X_logical = np.array([[-1, -1], [1, -1], [-1, 1], [1, 1]])
-    outputs = {
-        'AND': np.array([-1, -1, -1, 1]),
-        'XOR': np.array([-1, 1, 1, -1])
-    }
-    
-    selected_function = config_file.get('function', 'XOR')
+    outputs = {"AND": np.array([-1, -1, -1, 1]), "XOR": np.array([-1, 1, 1, -1])}
+
+    selected_function = config_file.get("function", "XOR")
     y_selected = outputs.get(selected_function)
 
-
     perceptron = PerceptronSimple(
-        seed=config_file.get('seed', 42),
+        seed=config_file.get("seed", 42),
         num_features=X_logical.shape[1],
-        learning_rate=config_file.get('learning_rate', 0.01),
-        epsilon=config_file.get('epsilon', 1e-5),
+        learning_rate=config_file.get("learning_rate", 0.01),
+        epsilon=config_file.get("epsilon", 1e-5),
     )
 
-    num_epochs = config_file.get('epochs', 50)
-    perceptron.train(X_logical, y_selected, num_epochs)
+    num_epochs = config_file.get("epochs", 50)
+    perceptron.fit(X_logical, y_selected, num_epochs)
 
     y_pred = perceptron.predict(X_logical)
 
-
-    with imageio.get_writer('./output/ej1/perceptron_training_2d_projected.gif', mode='I', duration=0.5) as writer_2d:
+    with imageio.get_writer(
+        "./output/ej1/perceptron_training_2d_projected.gif", mode="I", duration=0.5
+    ) as writer_2d:
         for epoch, weights in enumerate(perceptron.weights_history):
-            img_data = plot_2d_projected_decision_boundary(weights, X_logical, y_selected, epoch, config_file)
+            img_data = plot_2d_projected_decision_boundary(
+                weights, X_logical, y_selected, epoch, config_file
+            )
             image = imageio.imread(img_data)
             writer_2d.append_data(image)
 
--- tp3/src/ej2_non_linear.py
+++ tp3/src/ej2_non_linear.py
@@ -5,36 +5,38 @@ from models.perceptrons.perceptron_non_linear import PerceptronNonLinear
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 
+
 def logistic_regression(config_file):
     # Load Config
-    with open(config_file, 'r') as file:
+    with open(config_file, "r") as file:
         config_file = json.load(file)
 
-    function = config_file.get('non_linear_fn')
-    if (function == 'sigmoid'):
+    function = config_file.get("non_linear_fn")
+    if function == "sigmoid":
         scaler_sigmoid = MinMaxScaler(feature_range=(0, 1))
         X = scaler_sigmoid.fit_transform(X)
         y = scaler_sigmoid.fit_transform(y.reshape(-1, 1)).ravel()
-    elif (function == 'tanh'):
+    elif function == "tanh":
         scaler_tanh = MinMaxScaler(feature_range=(-1, 1))
         X = scaler_tanh.fit_transform(X)
         y = scaler_tanh.fit_transform(y.reshape(-1, 1)).ravel()
 
     # Load Dataset
     df = pd.read_csv("../res/TP3-ej2-conjunto.csv")
-    X = df[['x1', 'x2', 'x3']].values
-    y = df['y'].values
+    X = df[["x1", "x2", "x3"]].values
+    y = df["y"].values
 
     perceptron_non_linear = PerceptronNonLinear(
-        seed=config_file.get('seed', 0),
+        seed=config_file.get("seed", 0),
         num_features=X.shape[1],
-        learning_rate=config_file.get('learning_rate', 0.01),
-        epsilon=config_file.get('epsilon', 1e-5),
-        non_linear_fn=config_file.get('non_linear_fn', 'sigmoid'),
-        beta=config_file.get('beta', 0.9)
+        learning_rate=config_file.get("learning_rate", 0.01),
+        epsilon=config_file.get("epsilon", 1e-5),
+        non_linear_fn=config_file.get("non_linear_fn", "sigmoid"),
+        beta=config_file.get("beta", 0.9),
     )
 
-    perceptron_non_linear.train(X, y, config_file.get('epochs', 100))
+    perceptron_non_linear.fit(X, y, config_file.get("epochs", 100))
+
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
